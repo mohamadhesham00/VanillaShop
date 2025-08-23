f4fea387dc76a75512e317b651263a4c49d5b105
mohamadhesham00 <mohamadhesham112@gmail.com>
2025-08-23 07:45:21 +0300

    Ref: made a reusable main.js module to reuse it

--- JS/main.js
+++ JS/main.js
@@ -1,49 +1,57 @@
 import { isAuthenticated, removeCookie } from "./cookies.js";
 const toggleBtn = document.getElementById("profileDropdown");
 const menu = document.getElementById("dropdownMenu");
-let viewProfileBtn = document.getElementsByClassName("view-profile-btn")[0];
+const viewProfileBtn = document.getElementsByClassName("view-profile-btn")[0];
 const products = await loadProducts();
+const loading = document.getElementById("page-loading");
 
-// Toggle dropdown on click
-toggleBtn.addEventListener("click", function (e) {
-  e.preventDefault();
-  menu.classList.toggle("show");
-});
+async function init() {
+  try {
+    // Show/hide navbar based on authentication
+    toggleNavBar();
 
-// Close when clicking outside
-document.addEventListener("click", function (e) {
-  if (!toggleBtn.contains(e.target) && !menu.contains(e.target)) {
-    menu.classList.remove("show");
+    // Toggle profile dropdown
+    toggleBtn.addEventListener("click", (e) => {
+      e.preventDefault();
+      menu.classList.toggle("show");
+    });
+    document.addEventListener("click", (e) => {
+      if (!toggleBtn.contains(e.target) && !menu.contains(e.target)) {
+        menu.classList.remove("show");
+      }
+    });
+  } finally {
+    // Hide spinner after everything is loaded
+    loading.style.display = "none";
   }
-});
+}
 
-window.onload = async () => {
-  // For controlling what to show and what to hide in navbar based on authenticated & guest modes
+function toggleNavBar() {
   if (isAuthenticated()) {
-    let authenticated = document.getElementsByClassName("authenticated");
-    [...authenticated].forEach((element) => element.classList.remove("d-none"));
+    const authenticated = document.getElementsByClassName("authenticated");
+    [...authenticated].forEach((el) => el.classList.remove("d-none"));
 
-    let guest = document.getElementsByClassName("guest");
-    [...guest].forEach((element) => element.classList.add("d-none"));
-    // Add events on logout
-    let logoutBtn = document.getElementsByClassName("logout-btn")[0];
+    const guest = document.getElementsByClassName("guest");
+    [...guest].forEach((el) => el.classList.add("d-none"));
+
+    const logoutBtn = document.getElementsByClassName("logout-btn")[0];
     logoutBtn.addEventListener("click", () => {
       removeCookie("email");
       removeCookie("password");
       location.reload();
     });
   } else {
-    // Attach events on login
     let loginBtn = document.getElementsByClassName("login-btn")[0];
-
     loginBtn.addEventListener("click", () =>
       window.location.assign("auth.html")
     );
   }
-};
+}
 
 async function loadProducts() {
   const response = await fetch("../products.json");
   const products = await response.json();
   return products;
 }
+
+export { init, products };
