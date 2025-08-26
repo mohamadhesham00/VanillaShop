5e8ff88fc5d56954c3eb30522c8135cec9b86d88
mohamadhesham00 <mohamadhesham112@gmail.com>
2025-08-26 16:59:22 +0300

    Feat: products page with filtration

---
+++ JS/products.js
@@ -0,0 +1,102 @@
+import { init, products } from "./main.js";
+let productContainer = document.getElementsByClassName("product-container")[0];
+let categoriesFilterList = document.getElementsByClassName(
+  "category-filter-list"
+)[0];
+let priceRange = document.getElementsByClassName("price-range")[0];
+let startPriceSpan = document.getElementsByClassName("start-price")[0];
+let endPriceSpan = document.getElementsByClassName("end-price")[0];
+let filterBtn = document.getElementsByClassName("filter-btn")[0];
+window.onload = async () => {
+  try {
+    await init();
+    loadProducts();
+    let categories = loadProductCategories();
+    categories.forEach((category) => {
+      let box = `<label class="filter-option"
+      ><input type="checkbox" name="category" value = "${category}" /> ${category}</label
+      >`;
+      categoriesFilterList.innerHTML += box;
+    });
+    let minPrice = getMinPrice();
+    let maxPrice = getMaxPrice();
+    priceRange.setAttribute("min", minPrice);
+    priceRange.setAttribute("max", maxPrice);
+    priceRange.setAttribute("value", maxPrice);
+    startPriceSpan.innerHTML = "$" + minPrice;
+    endPriceSpan.innerHTML = "$" + maxPrice;
+  } finally {
+  }
+};
+priceRange.addEventListener("change", function () {
+  endPriceSpan.innerHTML = priceRange.value;
+});
+
+filterBtn.addEventListener("click", () => {
+  let filtered = products;
+  const checkedBoxes = document.querySelectorAll(
+    'input[name="category"]:checked'
+  );
+  if (checkedBoxes.length)
+    filtered = filterByCategory([...checkedBoxes].map((box) => box.value));
+  filtered = filterByPrice(priceRange.value, filtered);
+  loadProducts(filtered);
+});
+
+function loadProducts(filtered = products) {
+  productContainer.innerHTML = "";
+  filtered.forEach((product) => {
+    const card = `
+  <div class="col-6 col-md-4 col-lg-3">
+    <div class="card h-100 shadow-sm border-0">
+      <img src="${product.images[0]}" class="card-img-top img-fluid product-img" alt="${product.title}" data-id="${product.id}" />
+      <div class="card-body d-flex flex-column">
+        <h6 class="card-title">${product.title}</h6>
+        <p class="card-text mb-2 fw-bold">$${product.price}</p>
+        <div class="mt-auto">
+          <a href="#" class="btn btn-dark rounded-pill w-100">See Details</a>
+        </div>
+      </div>
+    </div>
+  </div>
+`;
+    productContainer.innerHTML += card;
+  });
+  // Initialize image hover effects after cards are added
+  const images = document.querySelectorAll(".product-img");
+  images.forEach((img) => {
+    let interval;
+    let product = products.find((product) => product.id == img.dataset.id);
+    img.addEventListener("mouseover", () => {
+      console.log("entered");
+      let i = 0;
+      interval = setInterval(() => {
+        img.src = product.images[i++];
+        i = i % product.images.length;
+      }, 850);
+    });
+
+    img.addEventListener("mouseout", () => {
+      clearInterval(interval);
+      img.src = product.images[0];
+    });
+  });
+}
+
+function loadProductCategories() {
+  return [...new Set(products.map((product) => product.category))];
+}
+function getMinPrice() {
+  let prices = products.map((product) => Number(product.price));
+  return Math.min(...prices);
+}
+function getMaxPrice() {
+  let prices = products.map((product) => Number(product.price));
+  return Math.max(...prices);
+}
+function filterByCategory(categories, filtered = products) {
+  return filtered.filter((product) => categories.includes(product.category));
+}
+function filterByPrice(maxPrice, filtered = products) {
+  return filtered.filter((product) => product.price <= maxPrice);
+}
